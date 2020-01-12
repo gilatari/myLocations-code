0bcf9b5ba0d70d13afe5d9dd89247350db9a4fd6
Gil Atari <gil.atari@gmail.com>
2020-01-12 20:57:14 +0200

    Updates

---
+++ precache-manifest.0b1a4bc50f4cb97a9689d38e2f4e4a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cff4d8ba5791496afd55613f50ac12e",
+    "url": "/myLocations/index.html"
+  },
+  {
+    "revision": "b7ecedc816d3c803c656",
+    "url": "/myLocations/static/css/main.af916e11.chunk.css"
+  },
+  {
+    "revision": "d794bf9cf0e04a5c4dd5",
+    "url": "/myLocations/static/js/2.3fa49ca4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myLocations/static/js/2.3fa49ca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7ecedc816d3c803c656",
+    "url": "/myLocations/static/js/main.3c3d973e.chunk.js"
+  },
+  {
+    "revision": "cea8cbbdf18eac199dbf",
+    "url": "/myLocations/static/js/runtime-main.0ed8131d.js"
+  }
+]);
